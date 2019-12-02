f6b688b94883b2b8675a5aeb5d755dd0c65a804e
Lot Gállego <loterion@gmail.com>
2019-12-02 15:09:32 +0100

    add specs

---
+++ silentdual/src/components/specs/SpecSensoresImg.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SpecSensoresImg from "../image/spec-sensores.png";
+
+//Animación con Lottie
+const SpecSensoresIcons = () => {
+  return <div></div>;
+};
+
+export default SpecSensoresComp = () => {
+  return (
+    <div>
+      <img src={SpecSensoresImg} alt={} />
+      <SpecSensoresIcons />
+    </div>
+  );
+};
---
+++ silentdual/src/components/specs/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import fonts from "../../fonts";
+import colors from "../../colors";
+import Wrapper from "../grid/wrapper";
+import Row from "../grid/row";
+import Column from "../grid/column";
+import SpecSensores from "./SpecSensores";
+import SpecFuncionamiento from "./SpecFuncionamiento";
+import SpecEntradasAire from "./SpecEntradasAire";
+
+const Title = styled.h2`
+  color: rgb(0, 0, 0);
+  font-family: DINBold;
+  font-size: 33px;
+  height: 51px;
+  letter-spacing: 0px;
+  line-height: 39px;
+  text-align: center;
+  width: 792px;
+`;
+export default Specs = () => {
+  return (
+    <section id="specs">
+      <Wrapper>
+        <Row>
+          <Column xs={12}>
+            <Title>La única opción doblemente inteligente</Title>
+          </Column>
+        </Row>
+        <SpecSensores />
+        <SpecFuncionamiento />
+        <SpecEntradasAire />
+      </Wrapper>
+    </section>
+  );
+};
