1593e4b30e136c35e95171e5d14da6989542b4e9
Pep <pepdbm7@gmail.com>
2019-12-03 10:53:24 +0100

    Add header + navbar

--- silentdual/src/pages/index.js
+++ silentdual/src/pages/index.js
@@ -10,21 +10,6 @@ import Row from "../utils/grid/row"
 import Column from "../utils/grid/column"
 
 const IndexPage = () => (
-<<<<<<< HEAD
-	<Fragment>
-		<SEO title="Home" />
-		<NavBar />
-		<HeroVideo />
-		<h1>Hi people</h1>
-		<p>Welcome to your new Gatsby site.</p>
-		<p>Now go build something great.</p>
-		<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-			<Image />
-		</div>
-		<Link to="/page-2/">Go to page 2</Link>
-	</Fragment>
-);
-=======
 
   <Layout>
 
@@ -35,6 +20,5 @@ const IndexPage = () => (
   </Layout>
 
 )
->>>>>>> 762bebcb324bb812045fcfe3d603ba218da7ef8b
 
 export default IndexPage;
