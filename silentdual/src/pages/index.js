9abab71f68ebf3d394ae64587e66884ff6713a62
Pep <pepdbm7@gmail.com>
2019-12-03 13:36:26 +0100

    added changes

--- silentdual/src/pages/index.js
+++ silentdual/src/pages/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Layout from "../components/layout";
 import Specs from "../components/specs";
+import NavBar from "../components/navBar";
+import Hero from "../components/hero";
 
 import Wrapper from "../utils/grid/wrapper";
 //import Row from "../utils/grid/row"
 //import Column from "../utils/grid/column"
 
 const IndexPage = () => (
-  <Layout>
-    <Wrapper></Wrapper>
-    <Specs />
-  </Layout>
+	<Layout className="layout" style={{ width: "100vw" }}>
+		<NavBar />
+		<Hero />
+		<Wrapper>
+			<Specs />
+		</Wrapper>
+	</Layout>
 );
 
 export default IndexPage;
