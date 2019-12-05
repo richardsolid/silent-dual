881dec333b1fff24e40f3acaf52f1deed8e81eaf
Quentin Bucciarelli <quentin.bucciarelli@binaria.com>
2019-12-05 15:15:44 +0100

    Fix modal Video player

--- silentdual/src/components/videoPlayer/index.js
+++ silentdual/src/components/videoPlayer/index.js
@@ -7,107 +7,107 @@ import videoPoster from "../../images/video_poster.jpg";
 import crossIcon from "../../images/cross.svg";
 
 const Background = styled.span`
-  position: fixed;
-  width: 100vw;
-  height: 100vh;
-  background: rgba(0, 0, 0, 0.9);
-  top: 0;
-  z-index: 10000;
-  display: flex;
+	position: fixed;
+	width: 100vw;
+	height: 100vh;
+	background: rgba(0, 0, 0, 0.9);
+	top: 0;
+	z-index: 10000;
+	display: flex;
 
-  opacity: 0;
-  visibility: hidden;
+	opacity: 0;
+	visibility: hidden;
 
-  video[poster] {
-    object-fit: cover;
-  }
+	video[poster] {
+		object-fit: cover;
+	}
 `;
 
 const VideoContainer = styled.div`
-  position: relative;
-  margin: auto;
-  max-width: 800px;
-  width: 100%;
-  z-index: 2000;
-  display: flex;
-  align-items: center;
+	position: relative;
+	margin: auto;
+	max-width: 800px;
+	width: 100%;
+	z-index: 2000;
+	display: flex;
+	align-items: center;
 `;
 
 const HomeVideo = styled.video`
-  position: relative;
-  width: inherit;
+	position: relative;
+	width: inherit;
 `;
 
 const CloseButton = styled.span`
-  position: absolute;
-  right: 8px;
-  top: 8px;
-  width: 20px;
-  height: 20px;
-  z-index: 10000;
-  background: url(${crossIcon}) no-repeat center center;
-  background-size: 100%;
-  cursor: pointer;
+	position: absolute;
+	right: 8px;
+	top: 8px;
+	width: 20px;
+	height: 20px;
+	z-index: 10000;
+	background: url(${crossIcon}) no-repeat center center;
+	background-size: 100%;
+	cursor: pointer;
 
-  &:hover {
-    opacity: 0.8;
-  }
+	&:hover {
+		opacity: 0.8;
+	}
 `;
 
 const VideoPlayer = ({ showVideo, width, video, setShowVideo, id }) => {
-  const scrollWorking = () => {
-    document.getElementsByTagName("body")[0].classList.toggle("scrollDisabled");
-  };
+	const scrollWorking = () => {
+		document.getElementsByTagName("body")[0].classList.toggle("scrollDisabled");
+	};
 
-  const showingVideo = () => {
-    gsap.to("#" + id, {
-      duration: 1,
-      opacity: 1,
-      visibility: "visible"
-    });
-    scrollWorking();
-  };
+	const showingVideo = () => {
+		gsap.to("#" + id, {
+			duration: 1,
+			opacity: 1,
+			visibility: "visible"
+		});
+		scrollWorking();
+	};
 
-  const closingVideo = () => {
-    gsap.to("#" + id, {
-      duration: 1,
-      opacity: 0,
-      visibility: "hidden"
-    });
-    scrollWorking();
-  };
+	const closingVideo = () => {
+		gsap.to("#" + id, {
+			duration: 1,
+			opacity: 0,
+			visibility: "hidden"
+		});
+		scrollWorking();
+	};
 
-  useEffect(() => {
-    showVideo ? showingVideo() : closingVideo();
-  });
+	useEffect(() => {
+		showVideo ? showingVideo() : closingVideo();
+	}, [showVideo]);
 
-  const closePlayer = () => {
-    setShowVideo(false);
-  };
+	const closePlayer = () => {
+		setShowVideo(false);
+	};
 
-  const isPhoneSize = width < 768;
+	const isPhoneSize = width < 768;
 
-  if (showVideo)
-    return (
-      <Background id={id}>
-        <VideoContainer>
-          <HomeVideo
-            src={video}
-            poster={videoPoster}
-            playsinline={!isPhoneSize}
-            controls={true}
-            type="video/mp4"
-            crossOrigin="anonymous"
-            muted
-          >
-            {!isPhoneSize && <source src={video} type="video/mp4" />}
-            Your browser can't play this video
-          </HomeVideo>
-          <CloseButton onClick={closePlayer} />
-        </VideoContainer>
-      </Background>
-    );
-  return null;
+	if (showVideo)
+		return (
+			<Background id={id}>
+				<VideoContainer>
+					<HomeVideo
+						src={video}
+						poster={videoPoster}
+						playsinline={!isPhoneSize}
+						controls={true}
+						type="video/mp4"
+						crossOrigin="anonymous"
+						muted
+					>
+						{!isPhoneSize && <source src={video} type="video/mp4" />}
+						Your browser can't play this video
+					</HomeVideo>
+					<CloseButton onClick={closePlayer} />
+				</VideoContainer>
+			</Background>
+		);
+	return null;
 };
 
 export default VideoPlayer;
