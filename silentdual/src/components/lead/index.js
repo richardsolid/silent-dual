4c88c3431fd82165897b7459c7e3e6f0bf8ac240
Lot Gállego <loterion@gmail.com>
2019-12-05 12:26:35 +0100

    lead WIP

--- silentdual/src/components/lead/index.js
+++ silentdual/src/components/lead/index.js
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useSpring, animated } from "react-spring";
+import useIntersect from "../../utils/useIntersect";
 
 //utils:
 import useWindowSize from "../../utils/useWindowSize";
-
-//grid:
-import Wrapper from "../../utils/grid/wrapper";
-import Row from "../../utils/grid/row";
-import Column from "../../utils/grid/column";
 
 //styles:
 import { breakpoints } from "../../assets/styles/breakpoints";
@@ -22,160 +18,172 @@ import LegImg from "../../images/cama_imatge_promo.png";
 import HeadImg from "../../images/cap_imatge_promo.png";
 import BathImg from "../../images/banyera_imatge_promo.png";
 
+const Parallax = styled.div``;
+const Fixed = styled(animated.div)`
+  top: 0;
+  position: fixed;
+  overflow: hidden;
+`;
 const LeadSection = styled.section`
-	height: 100vh;
-	width: 100vw;
-	display: flex;
-	align-items: center;
-	background-color: gray;
-	position: relative;
-	color: white;
-	overflow: hidden;
+  height: auto;
+  position: relative;
+  color: white;
+  overflow: hidden;
+  line-height: 0;
+  @media screen and (min-width: ${breakpoints.tablet}px) {
+    margin-bottom: 100%;
+    height: 2000px;
+  }
 
-	img {
-		object-fit: cover;
-		min-width: 100%;
-		min-height: 100%;
-		bottom: 0;
-		left: 0;
-		right: 0;
-		height: 100vh;
-		width: 100vw;
+  img {
+    width: 100%;
+    &.leg {
+      z-index: 2;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
 
-		position: absolute;
-		border-radius: 5px;
-		background-size: cover;
-		background-position: center center;
-		background-repeat: no-repeat;
-		will-change: transform;
+    &.head {
+      z-index: 1;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
 
-		&.leg {
-			z-index: 2;
-			left: -3%;
-			bottom: 6%;
-		}
-
-		&.head {
-			z-index: 1;
-			bottom: 7%;
-			left: 9%;
-		}
-
-		&.bath {
-			z-index: 3;
-			bottom: 20%;
-			left: 14%;
-		}
-	}
-
-	&:before {
-		content: "";
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		right: 0;
-		top: 0;
-		z-index: 1;
-		height: 100vh;
-		width: 100vw;
-
-		background-color: #000;
-		opacity: 0.6;
-	}
+    &.bath {
+      z-index: 3;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  }
 `;
 
 const BackgroundImage = styled.img`
-	z-index: 0;
-	top: 0 !important;
+  width: 100%;
 `;
 
-const Description = styled.h2`
-	font-family: DINBold;
-	font-size: 28px;
-	line-height: 34px;
-	text-align: center;
-	position: absolute;
-	z-index: 1000;
-	width: 80%;
-	@media screen and (min-width: ${breakpoints.tablet}px) {
-		font-size: 33px;
-		line-height: 39px;
-	}
+const Description = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+  h2 {
+    max-width: 70%;
+    font-weight: bold;
+    font-size: 28px;
+    line-height: 34px;
+    text-align: center;
+    @media screen and (min-width: ${breakpoints.tablet}px) {
+      max-width: 30%;
+      font-size: 33px;
+      line-height: 39px;
+    }
+  }
 `;
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
-const legtrans = (x, y) => `translate3d(${x / 20}px,${y / 10}px,0) scale(1.1)`;
-const headtrans = (x, y) => `translate3d(${x / 25}px,${y / 50}px,0) scale(1.4)`;
-const bathtrans = (x, y) => `translate3d(${x / 20}px,${y / 80}px,0) scale(1.5)`;
+const legtrans = (x, y) => `translate3d(${x / 40}px,${y / 30}px,0) scale(1.05)`;
+const headtrans = (x, y) =>
+  `translate3d(${x / 60}px,${y / 40}px,0) scale(1.05)`;
+const bathtrans = (x, y) =>
+  `translate3d(${x / 50}px,${y / 30}px,0) scale(1.05)`;
 
 const Lead = () => {
-	const widthWindow = useWindowSize();
+  const { format } = new Intl.NumberFormat("en-US", {
+    maximumFractionDigits: 2
+  });
 
-	const { lead } = data;
+  const buildThresholdArray = () => Array.from(Array(100).keys(), i => i / 100);
+  //useIntersect devulve ref y entry. ref es la referencia del elemento del cual queremos controlar su visualización en el viewport
+  //entry es el objeto con la información de la posición del elemento
+  const [ref, entry] = useIntersect({
+    //threshold es la cantidad de elemento visible para que se dispare el evento
+    threshold: buildThresholdArray()
+  });
 
-	const [width, setWidth] = useState(null);
+  const ratio = format(entry.intersectionRatio);
 
-	useEffect(() => {
-		setWidth(widthWindow.width);
-	}, [widthWindow]);
+  const parallaxProps = useSpring({
+    from: {
+      opacity: 0
+    },
+    to: {
+      opacity: (ratio - 0.1) * 8 || 0
+    }
+  });
 
-	const [props, set] = useSpring(() => ({
-		xy: [0, 0],
-		config: { mass: 10, tension: 550, friction: 140 }
-	}));
+  console.log(ratio);
 
-	if (width && typeof width === "number") {
-		if (width > breakpoints.large) {
-			return (
-				<LeadSection
-					onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}
-				>
-					<BackgroundImage src={BackgroundImg} alt="background" />
+  const widthWindow = useWindowSize();
 
-					<animated.img
-						className="leg"
-						src={LegImg}
-						alt="leg"
-						style={{ transform: props.xy.interpolate(legtrans) }}
-					/>
-					<animated.img
-						className="head"
-						src={HeadImg}
-						alt="head"
-						style={{ transform: props.xy.interpolate(headtrans) }}
-					/>
-					<animated.img
-						className="bath"
-						src={BathImg}
-						alt="bath"
-						style={{ transform: props.xy.interpolate(bathtrans) }}
-					/>
+  const { lead } = data;
 
-					<Wrapper>
-						<Row>
-							<Column xs={12}>
-								<Description>{lead.description}</Description>
-							</Column>
-						</Row>
-					</Wrapper>
-				</LeadSection>
-			);
-		} else {
-			return (
-				<LeadSection>
-					<BackgroundImage src={BackgroundImg} alt="background" />
-					<Wrapper>
-						<Row>
-							<Column xs={12}>
-								<Description>{lead.description}</Description>
-							</Column>
-						</Row>
-					</Wrapper>
-				</LeadSection>
-			);
-		}
-	}
-	return <div>loading...</div>;
+  const [width, setWidth] = useState(null);
+
+  useEffect(() => {
+    setWidth(widthWindow.width);
+  }, [widthWindow]);
+
+  const [props, set] = useSpring(() => ({
+    xy: [0, 0],
+    config: { mass: 10, tension: 50, friction: 40 }
+  }));
+
+  if (width && typeof width === "number") {
+    if (width > breakpoints.tablet) {
+      return (
+        <LeadSection
+          ref={ref}
+          onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}
+        >
+          <Fixed style={parallaxProps}>
+            <Parallax>
+              <BackgroundImage src={BackgroundImg} alt="background" />
+              <animated.img
+                className="leg"
+                src={LegImg}
+                alt="leg"
+                style={{ transform: props.xy.interpolate(legtrans) }}
+              />
+              <animated.img
+                className="head"
+                src={HeadImg}
+                alt="head"
+                style={{ transform: props.xy.interpolate(headtrans) }}
+              />
+              <animated.img
+                className="bath"
+                src={BathImg}
+                alt="bath"
+                style={{ transform: props.xy.interpolate(bathtrans) }}
+              />
+            </Parallax>
+            <Description>
+              <h2>{lead.description}</h2>
+            </Description>
+          </Fixed>
+        </LeadSection>
+      );
+    } else {
+      return (
+        <LeadSection>
+          <BackgroundImage src={BackgroundImg} alt="background" />
+          <Description>
+            <h2>{lead.description}</h2>
+          </Description>
+        </LeadSection>
+      );
+    }
+  }
+  return <div>loading...</div>;
 };
 
 export default Lead;
